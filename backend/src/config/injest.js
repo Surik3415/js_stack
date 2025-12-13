53d40e2a9ee88d6c80b09fa02c52d1d62b409ab3
Suren Avakian <suren.a@devmood.tech>
2025-12-13 21:14:41 +0200

    rework typo

--- backend/src/config/injest.js
+++ backend/src/config/injest.js
@@ -26,7 +26,7 @@ const syncUser = inngest.createFunction(
 
 const deleteUserFromDB = inngest.createFunction(
   { id:"delete-user-from-db" },
-  { event: "clerk/user.delete"},
+  { event: "clerk/user.deleted"},
   async({ event }) => {
     await connectDB()
 
